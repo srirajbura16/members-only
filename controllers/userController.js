29ef5f7a2a1d562b4cd9aa4d348bae49a4b4ed75
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-02 09:11:21 -0400

    implemented user controller and routes

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const User = require('../models/user');
+
+exports.login = (req, res) => {
+  res.send('NOT IMPLEMENTED: login page');
+};
+
+exports.signup = (req, res) => {
+  res.send('NOT IMPLEMENTED: sign-up page');
+};
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 
 //Controller Modules
 const post_controller = require('../controllers/postController');
+const user_controller = require('../controllers/userController');
 
 //Will be able to view without signing in.
 router.get('/', post_controller.posts);
 
-router.get('/sign-up');
+router.get('/sign-up', user_controller.signup);
 
-router.get('/login');
+router.get('/login', user_controller.login);
 
 //After login
 router.get('/membership');
