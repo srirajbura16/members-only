5bc619a68907e74cefda831e6cd18901b18681e5
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-02 08:42:47 -0400

    implemented post model

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+//Post URL
+PostSchema.virtual('url').get(() => {
+  return '/post/' + this._id;
+});
+
+module.exports = mongoose.model('Post', PostSchema);
--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
-  password: { required: true },
+  password: { type: String, required: true },
   membership_status: false,
   admin: false,
 });
