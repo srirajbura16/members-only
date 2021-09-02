2614f6c6b612a4d17ac3309e2c625bd6c9ba2971
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-02 09:08:17 -0400

    implemented post controller and routes

---
+++ controllers/postController.js
@@ -0,0 +1,5 @@
+const Post = require('../models/post');
+
+exports.posts = (req, res) => {
+  res.send('NOT IMPLEMENTED: Post list');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//Controller Modules
+const post_controller = require('../controllers/postController');
+
+//Will be able to view without signing in.
+router.get('/', post_controller.posts);
+
+router.get('/sign-up');
+
+router.get('/login');
+
+//After login
+router.get('/membership');
+
+router.get('/adminaccess');
 
 module.exports = router;
