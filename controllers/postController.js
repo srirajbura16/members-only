6736cdc8c52f95c69cac60cb302e969009ebebfa
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-06 14:10:35 -0400

    implemented post functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,13 +1,23 @@
 const Post = require('../models/post');
 
-exports.posts = (req, res) => {
+exports.posts = (req, res, next) => {
   res.render('index', { user: req.user });
 };
 
-exports.post_create_get = (req, res) => {
-  res.render('post');
+exports.post_create_get = (req, res, next) => {
+  res.render('post_create');
 };
 
-exports.post_create_post = (req, res) => {
-  res.render('index', { user: req.user });
+exports.post_create_post = (req, res, next) => {
+  const post = new Post({
+    user: req.user._id,
+    title: req.body.title,
+    message: req.body.message,
+  }).save((err) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.redirect('/');
+  });
 };
